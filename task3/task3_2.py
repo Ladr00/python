33eeb0e57c467c8f64ad4bd8df2b528361ca5ba0
Ladr00 <52217415+Ladr00@users.noreply.github.com>
2023-07-02 11:29:32 +0300

    Update task3_2.py

--- task3/task3_2.py
+++ task3/task3_2.py
@@ -2,18 +2,20 @@ print("Введите слово маленькими буквами:")
 a = input()
 
 gls = {'a', 'e', 'i', 'o', 'u'}
-gls_f = 0
+gls_f = {gls_n: 0 for gls_n in gls}
 sgl = 0
 
 for letter in a:
-    if letter.isalpha():
-        if letter in gls:
-            gls_f += 1
-        else:
-            sgl += 1
+    if letter in gls:
+        gls_f[letter] += 1
+    elif letter.isalpha():
+        sgl += 1
 
-if gls_f == 0 or sgl == 0:
+word = sum(gls_f.values())
+
+if word == 0 or sgl == 0:
     print(False)
 else:
-    print("Количество гласных букв:", gls_f)
-    print("Количество согласных букв:", sgl)
+    print(f"Количество согласных: {sgl}")
+    for gls_n, count in gls_f.items():
+        print(f"Количество гласных '{gls_n}': {count}")
