5074f726365b9d64ebf2e6f2b4263a7a5ff26ae4
Ladr00 <52217415+Ladr00@users.noreply.github.com>
2023-08-14 18:27:35 +0300

    Add files via upload

---
+++ task14/task14.py
@@ -0,0 +1,9 @@
+def recurs(lst, index=0):
+    if index < len(lst):
+        print(lst[index])
+        recurs(lst, index + 1)
+    else:
+        print("Конец списка")
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+recurs(my_list)
