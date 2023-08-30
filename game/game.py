08e5ac26b69aaba2f563898851867395de987c69
Ladr00 <52217415+Ladr00@users.noreply.github.com>
2023-08-30 20:35:49 +0300

    Update game.py

--- game/game.py
+++ game/game.py
@@ -7,11 +7,12 @@ from helicopter import Helicopter as Helico
 from clouds import Clouds  as  Clouds
 
 
-TICK_SLEEP = 0.1
+TICK_SLEEP = 0.05
 TREE_UPDATE = 50
 CLOUDS_UPDATE = 100
 FIRE_UPDATE = 100
 MAP_W, MAP_H = 20, 10
+TREE_BONUS = 100
 tick = 1
 
 field = Map(MAP_W, MAP_H)
@@ -20,17 +21,30 @@ helico = Helico(MAP_W, MAP_H)
 
 MOVES = {'w': (-1, 0), 'd': (0, 1), 's': (1, 0), 'a': (0, -1)}
 def process_key(key):
-    global helico
+    global helico, clouds, field, tick
     c = key.char.lower()
     if c in MOVES.keys():
         dx, dy = MOVES[c][0], MOVES[c][1]
         helico.move(dx, dy)
-    if c == 'f':
-        data = {"helicopter": helico.export_data(), 
-                "clouds": clouds.exprort_data(), 
-                "field": field.export_data()}
+    elif c == 'f':
+        print("ok")
+        data = {
+            "helicopter": helico.export_data(),
+            "clouds": clouds.export_data(),
+            "field": field.export_data(),
+            "tick": tick
+        }
         with open("level.json", "w") as lvl:
             json.dump(data, lvl)
+            print("ok")
+    elif c == 'g':
+        with open("level.json", "r") as lvl:
+            data = json.load(lvl)
+            tick = data["tick"] or 1
+            helico.import_data(data["helicopter"])
+            field.import_data(data["field"])
+            clouds.import_data(data["clouds"])
+            
 
 listener = keyboard.Listener(
     on_press=None,
@@ -43,6 +57,7 @@ while True:
     os.system("cls")
     print(helico.x, helico.y)
     field.process_helicopter(helico, clouds)
+    field.update_fires(helico, tick) 
     helico.print_stats()
     field.print_map(helico, clouds)
     print("TICK", tick)
@@ -51,6 +66,6 @@ while True:
     if (tick % TREE_UPDATE == 0):
         field.generate_tree()
     if (tick % FIRE_UPDATE == 0):
-        field.update_fires()
+        field.update_fires(helico, tick)
     if (tick % CLOUDS_UPDATE == 0):
         clouds.update()
